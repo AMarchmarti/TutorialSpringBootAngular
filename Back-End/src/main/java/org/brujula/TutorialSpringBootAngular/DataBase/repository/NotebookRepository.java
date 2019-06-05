ce7b931658abc7bc6798e3a1a5526375cf9dacc7
Toni March <amarch@brujula.es>
2019-06-05 12:18:05 +0200

    Deleted project and started a new one

--- Back-End/src/main/java/org/brujula/TutorialSpringBootAngular/DataBase/repository/NotebookRepository.java
+++
@@ -1,9 +0,0 @@
-package org.brujula.TutorialSpringBootAngular.DataBase.repository;
-
-import org.brujula.TutorialSpringBootAngular.model.NoteBook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface NotebookRepository extends JpaRepository<NoteBook, UUID> {
-}
--- Back-End/src/main/java/org/brujula/TutorialSpringBootAngular/mail/FeedbackSender.java
+++
@@ -1,5 +0,0 @@
-package org.brujula.TutorialSpringBootAngular.mail;
-
-public interface FeedbackSender {
-    void sendFeedback(String from, String name, String feedback);
-}
