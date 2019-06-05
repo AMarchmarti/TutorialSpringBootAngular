1807881cf9ac0cf0e2c2634ada4f2c1e0cdda221
Toni March <amarch@brujula.es>
2019-06-05 11:07:47 +0200

    Created the project

---
+++ src/main/java/org/brujula/TutorialSpringBootAngular/TutorialSpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package org.brujula.TutorialSpringBootAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringBootAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/brujula/TutorialSpringBootAngular/TutorialSpringBootAngularApplicationTests.java
@@ -0,0 +1,16 @@
+package org.brujula.TutorialSpringBootAngular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorialSpringBootAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
