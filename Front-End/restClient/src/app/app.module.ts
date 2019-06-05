03c0b2a43611bef35f957e15de165873919caca7
Toni March <amarch@brujula.es>
2019-06-05 13:00:20 +0200

    Added HttpModules

--- Front-End/restClient/src/app/app.module.ts
+++ Front-End/restClient/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ const appRoutes: Routes= [
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
